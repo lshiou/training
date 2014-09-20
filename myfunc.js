7262de071519976b592750457dba129db635cd8f
Lawrence Shiou <lawrence.shiou@gmail.com>
2014-09-20 10:58:01 -0700

    1st check in to master

---
+++ myfunc.js
@@ -0,0 +1,26 @@
+/*
+This is a test file
+
+1st version checking in to master
+*/
+
+//function add the list of arguments, then square
+function sumSquare(){  
+    var sum = 0;    
+    for(var i=0; i<arguments.length; i++){
+        sum += arguments[i];
+    }
+/*  alternative1    
+    for(var i in arguments){
+        sum += arguments[i];
+    }
+*/
+/*  alternative2    
+arguments.foreach(function(value, index, array){
+	sum += value;
+});
+*/
+    return Math.square(sum);
+}
+
+
